bf756a0430339971cd5a7dc514e09b10470ecc21
jbelciglio <jbelciglio@gmail.com>
2018-10-23 22:32:22 -0700

    Updates

---
+++ precache-manifest.3fd0c29b25913bc7e38613f40cac0fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cca34fc8f8946b978cf",
+    "url": "/memory_game/./static/js/runtime~main.5cca34fc.js"
+  },
+  {
+    "revision": "863d48c84af4a06b2dc0",
+    "url": "/memory_game/./static/js/main.863d48c8.chunk.js"
+  },
+  {
+    "revision": "7ccad06e8a96e8ae6253",
+    "url": "/memory_game/./static/js/1.7ccad06e.chunk.js"
+  },
+  {
+    "revision": "863d48c84af4a06b2dc0",
+    "url": "/memory_game/./static/css/main.4a65e63e.chunk.css"
+  },
+  {
+    "revision": "f5af025eb0cbbe0bd77888dcd22a3c79",
+    "url": "/memory_game/./index.html"
+  }
+];
